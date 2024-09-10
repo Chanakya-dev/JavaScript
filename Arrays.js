5bab51235a1a8338679e3077831ed36ce8c651ac
Balajikolla591 <balukolla91@gmail.com>
2024-09-10 16:20:44 +0530

    Balaji

--- Arrays.js
+++ Arrays.js
@@ -1,1 +1,10 @@
 // Load an Array with an Integer and Iterate them with  a Normal for Loop 
+let arr =[1,2,3,4,5]
+for (let a of arr){
+    console.log(a);
+}
+let i=0;
+while(i<arr.length){
+    console.log(i);
+    i++;
+}
--- Diff.js
+++ Diff.js
@@ -1,1 +1,24 @@
 // Differentiate let var and const by a simple programme
+let a=1;
+let b=2;
+{
+    let a=2;
+   console.log(a+b);
+}
+    console.log(a+b);
+
+var c=1;
+var d=2;{
+    var c=2;
+    console.log(c+d);   
+}
+        console.log(c+d);
+
+
+const e=1;
+const f=2;
+{
+    const e=2;
+    console.log(e+f);
+}
+        console.log(e+f);
