6a1b322b9c002539784fcd347bc433d348f2429e
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-09-10 19:01:05 +0530

    save

--- Diff.js
+++ Diff.js
@@ -1,1 +1,21 @@
 // Differentiate let var and const by a simple programme
+function testVariables() {
+  
+    if (true) {
+        var x = 1;
+        console.log("Inside if block - var x:", x); 
+    }
+    console.log("Outside if block - var x:", x); 
+
+    if (true) {
+        let y = 2;
+        console.log("Inside if block - let y:", y); 
+    }
+    if (true) {
+        const z = 3;
+        console.log("Inside if block - const z:", z); 
+    }
+}
+
+testVariables();
+
