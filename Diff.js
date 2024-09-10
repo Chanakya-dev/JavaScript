da329e50d972aafec57f04b1dc4f7cbcc127bab2
Aparna526 <aparnakondapaturu@gmail.com>
2024-09-10 11:59:11 +0530

    one

--- Diff.js
+++ Diff.js
@@ -1,1 +1,26 @@
 // Differentiate let var and const by a simple programme
+let a=10;
+const b=20;
+var c=30;
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("Updating the values")
+
+a=30;
+c=25;
+//b=10; //This cannot be updated. It gives an error.
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("Difference between let and var")
+
+function prod(){
+    let x=10;
+    var y=5;
+    console.log(x*y)
+}
+prod();
+
--- Objects.js
+++ Objects.js
@@ -1,1 +1,2 @@
 // Prepare an Object with a String,int,function and Object in it and itereate Print Names from the Inner Object
+
