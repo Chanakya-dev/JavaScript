9df90b66f9ee9cccf824752e62fbce0324ec5514
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-09-10 18:29:21 +0530

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -1,1 +1,23 @@
 // Prepare an Object with a String,int,function and Object in it and itereate Print Names from the Inner Object
+
+let employee = {
+  name: "John",
+  age: 30,
+  salary: 20000,
+  address: { city: "New York", state: "NY" },
+  display: function () {
+    console.log(this.name);
+  },
+};
+
+for (let key in employee) {
+  if (typeof employee[key] === "object") {
+    for (let innerKey in employee[key]) {
+      console.log(employee[key][innerKey]);
+    }
+  } else if (typeof employee[key] === "function") {
+    employee[key]();
+  } else {
+    console.log(employee[key]);
+  }
+}
