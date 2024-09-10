f1cd3e42822887eca1261154d661502cd8b8470a
vijay767492 <vijaydandu59@gmail.com>
2024-09-10 17:54:59 +0530

    vijay dandu

--- Arrays.js
+++ Arrays.js
@@ -1,1 +1,5 @@
 // Load an Array with an Integer and Iterate them with  a Normal for Loop 
+let a=[23,5,76,1,8,0,"vijay"];
+for(let i=0;i<a.length;i++){
+    console.log(a[i]);
+}
--- Loops.js
+++ Loops.js
@@ -1,2 +1,26 @@
 // Take an Array of Objects and Iterate them using for of and for in and While Loops 
 // Prepare an Array With Set of Objects and Iterate them using Map function
+let array=[{id:48,phone:767493,name:"vijay"},{id:45,phone:965302,name:"shubam"}]
+console.log("using map")
+let listt = array.map(ar => array);
+console.log(listt);
+console.log(" Iterating using for of ")
+for (let ss of array) {
+    console.log(ss);}
+    console.log(" Iterating using for in ")
+for(let tt in array){
+    console.log(array[tt]);
+}
+console.log("iterating by using the while loop");
+let i=0;
+while(i<array.length){
+    console.log(array[i]);
+    i++;
+}
+
+  
+  
+
+
+
+
