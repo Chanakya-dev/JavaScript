9b80227566fcd9acfa2a67a534c8ecdd25c95d32
Shoebmirza00 <shoebbaig31@gmail.com>
2024-09-10 17:51:03 +0530

    done

--- Loops.js
+++ Loops.js
@@ -1,2 +1,23 @@
 // Take an Array of Objects and Iterate them using for of and for in and While Loops 
 // Prepare an Array With Set of Objects and Iterate them using Map function
+let obj =[
+    {
+        id:1,
+        name:"shoeb",
+        add :function m(){
+            a+b;
+        }
+    },
+    {
+        id:2,
+        name:"mirza"
+    }
+]
+for (let m in obj){
+    console.log(obj[m]);
+}
+console.log("-------------------------------------------------");
+
+for (let m of obj){
+    console.log([m]);
+}
