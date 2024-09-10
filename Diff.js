a9a3cf40c3ff7e6e7c4842044cbfe10ccd27ec53
karthhiik <karthikthatikonda18@gmail.com>
2024-09-10 19:24:33 +0530

    Update Diff.js

--- Diff.js
+++ Diff.js
@@ -1,1 +1,19 @@
 // Differentiate let var and const by a simple programme
+let a=10;
+if(a>0){
+  let a=70;
+  console.log(a);
+}
+console.log(a);
+const b=20;
+if(b>9){
+  const b=89;
+  console.log(b);
+}
+console.log(b);
+var p=65;
+if(p>0){
+  var p=90;
+  console.log(p);
+}
+console.log(p);
