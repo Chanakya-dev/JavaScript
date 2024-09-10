7a523d02bd5200712a419e3fc4ce22bf772f4c11
Talari-Lavanya <lavanya62811@gmail.com>
2024-09-10 16:56:33 +0530

    Lavanya

--- Diff.js
+++ Diff.js
@@ -1,1 +1,34 @@
 // Differentiate let var and const by a simple programme
+function f1(){
+    let a=20;
+    let b=30;
+    if(a<b){
+        c=25;
+         console.log(" let c : " + c);
+    }
+    console.log(" let a : " + a);
+}
+
+f1();
+
+function f2(){
+    var s=10;
+    var d=12;
+    console.log(" var s : " + s);
+    if(s>d){
+        var e=15;
+        console.log(" var e : " + e);
+    }
+    console.log(" vas s : " + s);
+    
+}
+f2();
+
+function f3(){
+    const k=3;
+    const y=4;
+    add=k+y;
+       console.log(" count add : " + add);
+    
+}
+f3();
--- Loops.js
+++ Loops.js
@@ -1,2 +1,49 @@
 // Take an Array of Objects and Iterate them using for of and for in and While Loops 
 // Prepare an Array With Set of Objects and Iterate them using Map function
+let person=[
+    {
+        name:"lavanya",
+        age:21
+    },
+    {
+        name:"ravathi",
+        age:22
+    }
+];
+console.log("for of");
+for(let x of person){
+    console.log(x);
+}
+
+console.log("for in");
+for(let x in person){
+    console.log(person[x]);
+}
+
+console.log("while loop");
+let i=0;
+while(i<person.length){
+console.log(person[i]);
+i++;
+}
+
+//array of objects
+let employee=[
+    {
+        age:23,
+        name:"Lav",
+        id:1
+    },
+    {
+        age:24,
+        name:"rev",
+        id:2
+    }
+];
+// console.log("using map function");
+// console.log(employee[1].name);
+// let p=employee.map(pov => pov.name);
+console.log("Using map function:");
+employee.map(employee => {
+    console.log(`ID: ${employee.id}, Name: ${employee.name}, Age: ${employee.age}`);
+});
